06acf4dcab83327bc79aed586985c923d389e00a
Alacho <havard_skole@hotmail.com>
2014-04-30 16:26:27 +0200

    Koblet til database
    Koblet til database

--- checklogin.php
+++ checklogin.php
@@ -10,7 +10,7 @@ $db_username="root";
 $db_password="";
 
 //Kobler til database og velger den
-/*$mysqli_con = mysqli_connect($db_host, $db_username, $db_password, $db_name);
-$mysqli_con->select_db("login") or die("Kunne ikke"); */
+$mysqli_con = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+$mysqli_con->select_db("login") or die("Kunne ikke");
 
 ?>
